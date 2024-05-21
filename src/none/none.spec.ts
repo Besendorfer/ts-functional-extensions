b8f241617c6e197f99e2e4a1907a1f0751adda33
Teancum Besendorfer <teancum@besendorfer.net>
2024-05-20 21:32:10 -0600

    Add predicate to none

--- src/none/none.spec.ts
+++ src/none/none.spec.ts
@@ -16,4 +16,20 @@ describe('none', () => {
 
     expect(result).toBe(false);
   });
+
+  it('should return true if all the elements in the array don\'t match the predicate', () => {
+    const arr = [0, 2, 4];
+
+    const result = arr.none(x => x % 2 === 1);
+
+    expect(result).toBe(true);
+  });
+
+  it('should return false if any of the elements in the array match the predicate', () => {
+    const arr = [0, 2, 3, 4];
+
+    const result = arr.none(x => x % 2 === 1);
+
+    expect(result).toBe(false);
+  });
 });
--- src/none/none.ts
+++ src/none/none.ts
@@ -2,21 +2,23 @@ export {}
 
 declare global {
   interface Array<T> {
-    none(): boolean;
+    none(cb?: (value: T, index: number, array: T[]) => unknown): boolean;
   }
 }
 
 /**
  * none:
  *
- * `none` is a method that allows you to check if an array has no elements.
+ * `none` is a method that allows you to check if an array has no elements that match the predicate.
+ * If no predicate is provided, it simply checks if the array has no elements at all.
  *
  */
 
 if (!Array.prototype.none) {
   Object.defineProperty(Array.prototype, 'none', {
-    value: function none<T>(this: T[]): boolean {
-      return this.length === 0;
+    value: function none<T>(this: T[], cb?: (value: T, index: number, array: T[]) => unknown): boolean {
+      if (!cb) return this.length === 0;
+      else return !this.some(cb);
     }
   });
 }
